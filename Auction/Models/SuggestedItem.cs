5174644c29dc764477312639918e3e6c25765aaf
Kristiyana Gacheva <kristiyana1@gmail.com>
2016-11-13 18:18:00 +0200

    Fixed Name and Rating properties in SuggesteItem model.
    Made their string lengths coincide with the ones in Item model so that exceptions upon SuggestedItem approval are avoided.

--- Auction/Models/SuggestedItem.cs
+++ Auction/Models/SuggestedItem.cs
@@ -12,12 +12,14 @@ namespace Auction.Models
 
         [Required]
         [Display(Name = "Name")]
+        [StringLength(60, MinimumLength = 3)]
         public String Name { set; get; }
 
         [Display(Name = "Description")]
         public String Description { set; get; }
 
         [Display(Name = "Rating")]
+        [StringLength(5)]
         public string Rating { set; get; }
 
         [Display(Name = "Image")]
